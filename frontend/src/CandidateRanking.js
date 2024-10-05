c136e1d4744d35fdb390a89252d9041c173dd8a1
Varshith1909 <vc8@pdx.edu>
2024-10-05 03:22:30 -0700

    Minor update

--- frontend/src/CandidateRanking.js
+++ frontend/src/CandidateRanking.js
@@ -14,6 +14,7 @@ const CandidateRanking = () => {
     setLoading(true);
     setError(null);
     try {
+      //Updated the link for prod
       const response = await axios.post('https://backend-backend-hntl.onrender.com/rank-candidates', {
         requiredRole,
         topN,
